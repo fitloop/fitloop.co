ff35d2b4a0314f632bca1295b76d08382cafead3
Shridhar Gupta <shri90@gmail.com>
2014-06-18 00:21:15 -0700

    remove phantom and spiderable, fix minor errors.

---
+++ client/views/routines/log_workout.js
@@ -0,0 +1,32 @@
+Template.logWorkout.helpers({
+
+	isLogged : function() {
+		if(WorkoutTracker.find().count() > 0)
+			return true;
+		else
+			return false;
+	}
+
+});
+
+
+Template.logWorkout.events({
+	'click #track-workout' : function(e,t) {
+
+		var routineId = this._id;
+
+		if(Meteor.user())
+		{
+			Meteor.call('logWorkout', routineId)
+		}
+	},
+
+	'click #undo-track-workout' : function(e,t) {
+		var workoutLog = WorkoutTracker.findOne({routineId: this._id});
+
+		if(Meteor.user() && workoutLog)
+		{
+			Meteor.call('undoLogWorkout', workoutLog._id, new Date());
+		}
+	}
+});
--- client/views/routines/routine.js
+++ client/views/routines/routine.js
@@ -10,23 +10,3 @@ Template.routine.helpers({
 });
 
 
-Template.routine.events({
-	'click #track-workout' : function(e,t) {
-
-		var routineId = this._id;
-
-		if(Meteor.user())
-		{
-			Meteor.call('logWorkout', routineId)
-		}
-	},
-
-	'click #undo-track-workout' : function(e,t) {
-		var workoutLog = WorkoutTracker.findOne({routineId: this._id});
-
-		if(Meteor.user() && workoutLog)
-		{
-			Meteor.call('undoLogWorkout', workoutLog._id, new Date());
-		}
-	}
-});
